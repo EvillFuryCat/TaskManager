d31777afff29bb75a276099a4bec3d7e8ecd3abe
Konstantin <k1st.volkov@gmail.com>
2022-09-28 14:15:01 +0400

    Revert "add models task, tags. applied migrations"
    This reverts commit c81f60e515fca98f375d586c15f4675e2f6224c4.

--- task_manager/main/models/__init__.py
+++ task_manager/main/models/__init__.py
@@ -1,6 +1,6 @@
 from .user import User
-from .task import Task
-from .tags import Tags
 
 
-__all__ = ["User", "Task", "Tags"]
+__all__ = [
+    "User"
+]
--- task_manager/main/models/tags.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class Tags(models.Model):
-    title = models.CharField(max_length=20)
