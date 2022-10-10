dc4bdffdd4f59f834511c4169a70b1e4d19ba829
Konstantin <k1st.volkov@gmail.com>
2022-10-10 20:59:49 +0400

    add view User, Task, Tag

--- task_manager/main/views.py
+++ task_manager/main/views.py
@@ -1,11 +1,25 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from django.shortcuts import render
 
-from .serializers import TaskSerializer
-
-from .models import Task
+from .serializers import TaskSerializer, UserSerializer, TagSerializer
+from .models import Task, User
 
 
 class TaskAPIView(generics.ListAPIView):
     queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.order_by("id")
+    serializer_class = UserSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.prefetch_related("tags")
+    serializer_class = TagSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().select_related("executor").order_by("id")
     serializer_class = TaskSerializer
