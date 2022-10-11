4820c2c695536541dfb3d8878c04eda0db5470bb
Konstantin <k1st.volkov@gmail.com>
2022-10-11 20:01:27 +0400

    Added filters for Users and Tasks

--- task_manager/main/views.py
+++ task_manager/main/views.py
@@ -1,18 +1,30 @@
 from rest_framework import generics, viewsets
 from django.shortcuts import render
+import django_filters
+
 
 from .serializers import TaskSerializer, UserSerializer, TagSerializer
-from .models import Task, User
+from .models import Task, User, Tag
 
+#
 
 class TaskAPIView(generics.ListAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
 
+class UserFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr="icontains")
+
+    class Meta:
+        model = User
+        fields = ("name",)
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.order_by("id")
     serializer_class = UserSerializer
+    filterset_class = UserFilter
 
 
 class TagViewSet(viewsets.ModelViewSet):
@@ -20,6 +32,17 @@ class TagViewSet(viewsets.ModelViewSet):
     serializer_class = TagSerializer
 
 
+class TaskFilter(django_filters.FilterSet):
+    tags = django_filters.ModelMultipleChoiceFilter(field_name="tags__title", queryset=Tag.objects.all())
+    author = django_filters.CharFilter(lookup_expr="icontains")
+    executor = django_filters.CharFilter(lookup_expr="icontains")
+
+    class Meta:
+        model = Task
+        fields = ["tags", "author", "executor"]
+
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all().select_related("executor").order_by("id")
     serializer_class = TaskSerializer
+    filterset_class = TaskFilter
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
-from task_manager.main.views import TaskAPIView
+from task_manager.main.views import TaskAPIView, TaskViewSet
+
+router = routers.SimpleRouter()
+router.register(r"task", TaskViewSet)
 
 urlpatterns = [
     path("main/", include("task_manager.main.urls")),
     path("api/v1/tasklist", TaskAPIView.as_view()),
+    path("api/", include(router.urls)),
 ]
